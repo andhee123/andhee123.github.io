718d7ba527d23f4de1e0f8d2a7fd7c14dd665efa
andhee123 <andi120903@gmail.com>
2021-07-11 20:25:27 +0700

    mengubah selector class pada navigation

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ const navigation = document.querySelectorAll('ul li a');
 for (let i = 0; i < navigation.length; i++) {
 
   navigation[0].addEventListener('click', function() {
-    navigation[i].classList.remove('active', 'm-active');
-    navigation[0].classList.add('active', 'm-active');
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[0].classList.add('active', 'fw-bold');
   });
 
   navigation[1].addEventListener('click', function() {
-    navigation[i].classList.remove('active', 'm-active');
-    navigation[1].classList.add('active', 'm-active');
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[1].classList.add('active', 'fw-bold');
   });
 
   navigation[2].addEventListener('click', function() {
-    navigation[i].classList.remove('active', 'm-active');
-    navigation[2].classList.add('active', 'm-active');
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[2].classList.add('active', 'fw-bold');
   });
 
   navigation[3].addEventListener('click', function() {
-    navigation[i].classList.remove('active', 'm-active');
-    navigation[3].classList.add('active', 'm-active');
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[3].classList.add('active', 'fw-bold');
   });
 
 }
