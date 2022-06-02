309db97a402b73a501f58e9b89b3b9b777d0dfaf
andhee123 <andi120903@gmail.com>
2022-06-02 11:03:10 +0700

    merapikan file dan memasukan nya ke folder tertentu

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Navigation with DOM Manipulation -->
+
+const navigation = document.querySelectorAll('ul li a');
+
+for (let i = 0; i < navigation.length; i++) {
+
+  navigation[0].addEventListener('click', () => {
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[0].classList.add('active', 'fw-bold');
+  });
+
+  navigation[1].addEventListener('click', () => {
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[1].classList.add('active', 'fw-bold');
+  });
+
+  navigation[2].addEventListener('click', () => {
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[2].classList.add('active', 'fw-bold');
+  });
+
+  navigation[3].addEventListener('click', function() {
+    navigation[i].classList.remove('active', 'fw-bold');
+    navigation[3].classList.add('active', 'fw-bold');
+  });
+
+}
+
+const loveIcon = document.querySelector('footer p i');
+
+loveIcon.addEventListener('click', function () {
+  loveIcon.style.color = 'red';
+})
